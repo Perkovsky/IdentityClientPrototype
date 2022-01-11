52a6e590bfd9db02c12c968637bb8f00d620776c
Igor <igor.perkovsky@mail.ru>
2022-01-11 10:48:17 -0500

    Doesn't work correctly

--- IdentityClientPrototype/Program.cs
+++ IdentityClientPrototype/Program.cs
@@ -3,8 +3,11 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
+using IdentityClientPrototype.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddAuthentication(options =>
     {
@@ -17,6 +20,7 @@ builder.Services.AddAuthentication(options =>
         options.RequireHttpsMetadata = false;
         options.Authority = "https://localhost:7100";
         options.Audience = "https://localhost:7100";
+        options.BackchannelHttpHandler = new CustomBackchannelHttpHandler(builder.Services);
     });
 
 #region Swagger
