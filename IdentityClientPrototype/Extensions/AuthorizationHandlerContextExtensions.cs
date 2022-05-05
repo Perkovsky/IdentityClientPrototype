8ad3ceea8f1327f97e6a3662d46fc17a94a6acef
Igor <igor.perkovsky@mail.ru>
2022-05-05 17:10:33 -0400

    HasRole has a new approach

--- IdentityClientPrototype/Extensions/AuthorizationHandlerContextExtensions.cs
+++ IdentityClientPrototype/Extensions/AuthorizationHandlerContextExtensions.cs
@@ -1,27 +1,30 @@
 ﻿using System.Diagnostics;
-using System.IdentityModel.Tokens.Jwt;
+using System.Text.Json;
 using Microsoft.AspNetCore.Authorization;
-using static OpenIddict.Abstractions.OpenIddictConstants;
 
 namespace IdentityClientPrototype.Extensions
 {
     public static class AuthorizationHandlerContextExtensions
     {
+        private const string RolesKey = "roles";
+
         public static bool HasRole(this AuthorizationHandlerContext context, string role)
         {
             var sw = new Stopwatch();
             sw.Start();
 
+            if (context.HasFailed)
+                return false;
+
             if (!context.User.Identity?.IsAuthenticated ?? false)
                 return false;
 
-            if (context.Resource is not DefaultHttpContext httpContext)
+            var vRoles = context.User.Claims.FirstOrDefault(x => x.Type == RolesKey)?.Value;
+            if (string.IsNullOrWhiteSpace(vRoles))
                 return false;
 
-            var auth = httpContext.Request.Headers.Authorization.First();
-            var jwt = auth.Substring(7);
-            var token = new JwtSecurityTokenHandler().ReadJwtToken(jwt);
-            var result = token.Claims.Any(x => x.Type == Claims.Role && x.Value == role);
+            var roles = JsonSerializer.Deserialize<IEnumerable<string>>(vRoles);
+            var result = roles.Any(r => r == role);
 
             sw.Stop();
             Trace.WriteLine(sw.Elapsed);
